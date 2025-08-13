998e748a52483b02c03b518e86e1ba9b35412e07
Ryusen9 <asratul02@gmail.com>
2025-08-13 08:19:54 +0600

    feat: integrate Tailwind CSS, add routing, and restructure components
    - Added Tailwind CSS and related dependencies to the project.
    - Created a new routing structure using React Router.
    - Implemented Navbar and Footer components for layout consistency.
    - Introduced HomeLayout for the home page structure.
    - Removed App.css and migrated styles to Tailwind CSS.
    - Updated main entry point to use RouterProvider for routing.
    - Added a React logo SVG asset to the public directory.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router";
+import routers from "./Routes/Routes.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={routers}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </RouterProvider>
+);
