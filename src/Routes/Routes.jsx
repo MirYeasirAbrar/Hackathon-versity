92e2fe3bbf4536e0e8a596ef02795d3000ef28d4
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2025-08-13 10:28:21 +0600

    feat: add contact page and integrate email functionality

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import HomeLayout from "../Layouts/HomeLayout";
 import Login from "../components/Login'/Login";
 import Register from "../components/Register/Register";
+import Contact from "../components/Contact/Contact";
 
 const routers = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const routers = createBrowserRouter([
         path: "/register",
         element: <Register />,
       },
+      {
+        path: "/contact",
+        element: <Contact/>
+      }
     ],
   },
 ]);
